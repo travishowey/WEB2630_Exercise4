d0de882b29704f361c36382a631699858d1662fc
travishowey <77299383+travishowey@users.noreply.github.com>
2021-03-01 19:31:23 -0700

    Add files via upload

---
+++ components/Server/Server.vue
@@ -0,0 +1,25 @@
+<template>
+  <!-- ToDo: Inside the list item tag add a click event that calls serverSelected -->
+  <li class="list-group-item" @click="severSelected">
+    <p># {{ server.id }}</p>
+    <!-- ToDo: Output Server # {{ server.id }} -->
+  </li>
+</template>
+
+<script>
+import eventBus from '../../main';
+
+export default {
+  props: {
+    server: {
+      type:String
+    }
+  },
+  methods: {
+    serverSelected () {
+      eventBus.$emit('changeServer', this.serverSelected)
+      }
+    // ToDo: Create serverSelected that creates a custom event that sends the server data to the eventBus (tip: eventBus.$emit('customEventName', data))
+  }
+}
+</script>
---
+++ components/Server/ServerDetails.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="col-xs-12 col-sm-6">
+    <p v-if= !server>Server Details are currently not updated</p>
+    <p v-else># {{ server.id }} selected, Status: {{ server.status }} </p>
+    <hr>
+    <button @click="resetStatus">Change to Normal</button>
+  </div>
+</template>
+
+<script>
+import { eventBus } from '../../main.js'
+
+export default {
+  data () {
+    return {
+      server: 'null'
+    }
+  },
+  methods: {
+    resetStatus () {
+      this.serverStatus = 'normal'
+    }
+    
+    // ToDo: Create a method called resetStatus that takes server.status and sets it to a string of Normal
+  },
+  created () {
+    eventBus.$on('changeServer', (server) => {
+      this.serverStatus = server
+    })
+    // ToDo: Set the server variable from above equal to the the server data coming through the event bus using eventBus.$on('customEventName', (server) => {
+    //   set data here
+    // })
+  }
+}
+</script>
